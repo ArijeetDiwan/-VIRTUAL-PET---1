5ca16280291608535552d4c0ecef4f94dfa02a31
ArijeetDiwan <71815213+ArijeetDiwan@users.noreply.github.com>
2020-11-24 14:51:08 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,56 @@
-
-var food;
-var background;
+var dog,dogImg,dogImg1;
 var database;
-var dog;
+var foodS,foodStock;
+
 function preload(){
-  dogImage=addImage(dogimg.png);
-  dogImage=addImage(dogimg1.png);
-}
+   dogImg=loadImage("images/dogimg.png");
+   dogImg1=loadImage("images/dogimg1.png");
+  }
 
+//Function to set initial environment
 function setup() {
-	createCanvas(500,500);
-  database = firebase.database();
-  food = new Food();
+  database=firebase.database();
+  createCanvas(500,500);
+
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale=0.15;
+
+  foodStock=database.ref('food');
+  foodStock.on("value",readStock);
+  textSize(20); 
 }
 
-
-function draw() {  
-  background(black);
+// function to display UI
+function draw() {
+  background(46,139,87);
+ 
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg1);
+  }
 
   drawSprites();
-  
-
+  fill(255,255,254);
+  stroke("black");
+  text("Food remaining : "+foodS,170,200);
+  textSize(13);
+  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
 }
 
+//Function to read values from DB
+function readStock(data){
+  foodS=data.val();
+}
 
-
+//Function to write values in DB
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  } 
+  database.ref('/').update({
+    food:x
+  })
+}
